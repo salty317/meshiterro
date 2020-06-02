4e5ee14ce5014b13dc1f6f97877d7580aae2a804
Shion Tanaka <b8219@oic.jp>
2020-06-03 02:50:28 +0900

    [Update] users#showのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
